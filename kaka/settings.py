a713e55eba288c0c86ec5516113ea8200b492732
vvact <vbm7752@gmail.com>
2025-08-18 21:41:38 +0300

    new commit

--- kaka/settings.py
+++ kaka/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="127.0.0.1,localhost", cast=Csv())
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="127.0.0.1,localhost,kaka-mtfj.onrender.com", cast=Csv())
 
 
 # Application definition
