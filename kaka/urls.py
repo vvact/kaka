601c45d55a19743eb8fe9f2f5daa9ee44b74602b
vvact <vbm7752@gmail.com>
2025-08-24 01:39:05 +0300

    new commit

--- kaka/urls.py
+++ kaka/urls.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
+def root_view(request):
+    return JsonResponse({"message": "Welcome to GentlemanWell API"})
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', root_view),
     path('api/', include('products.urls')),  # or whatever your app is called
     path('api/', include('cart.urls')),  # Include cart URLs
     path('api/', include('moments.urls')),  # Include moments URLs
