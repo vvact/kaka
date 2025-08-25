9d27dc06e76e53076b6d90bd96de659003e97d66
vvact <vbm7752@gmail.com>
2025-08-25 15:09:24 +0300

    new commit

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-# Navigate to the script's directory
 cd "$(dirname "$0")" || exit
 
-# Pull latest code from GitHub
+# Pull latest code
 git reset --hard
 git pull origin master
 
-# Rebuild Docker containers
-docker-compose up -d --build
+# Step 1: Start nginx-proxy alone
+docker-compose up -d nginx-proxy
+echo "Waiting 5-10 seconds for nginx-proxy to initialize..."
+sleep 10
+
+# Step 2: Start the rest of the containers
+docker-compose up -d --build api celery_worker flower redis postgres-db
 
 # Optional: prune unused Docker images
 docker image prune -f
